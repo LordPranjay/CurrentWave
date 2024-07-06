2378402f2ae910f145fc626b8717a2c292fbb708
Pranjay Yadav <ypranjay@gmail.com>
2024-07-06 23:35:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from './components/About'
+import Navbar from './components/Navbar'
+import TopStories from './components/TopStories'
+import NotFound from './components/NotFound'
+import Footer from './components/Footer'
+import Search from './components/Search'
+import {categories} from './constants/index'
+
+function App() {
+  const apiKey = REACT_APP_CURRENT_WAVE;
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<TopStories apiKey={apiKey} section="home" />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+          <Route exact path='*' element={<NotFound />}></Route>
+          <Route exact path='/search' element={<Search apiKey={apiKey}/>}></Route>
+          {
+            categories.map((e) => {
+              return <Route key={e} exact path={`/categories/${e.toLowerCase()}`} element={<TopStories apiKey={apiKey} section={e.toLowerCase()} />}></Route>
+            })
+          }
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
